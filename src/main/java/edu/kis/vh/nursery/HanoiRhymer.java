c813465eb8df797abf5bba153118682f90a75150
lilyum-19 <229867@edu.p.lodz.pl>
2023-05-30 22:45:19 +0200

    17.utworzenie dokumentacji pliku HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,10 +4,16 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * @return number of rejections
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * @param in int variable being put on top of stack
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
